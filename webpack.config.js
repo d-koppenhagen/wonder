a437f27e7ef34b944afeec6f64ca5624b5ac9c53
Danny Koppenhagen <mail@d-koppenhagen.de>
2019-05-01 21:29:00 +0200

    Updates packages and ts / webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,28 @@
-var path = require('path');
-var webpack = require('webpack');
-var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const PATHS = {
-  src: path.join(__dirname, './src'),
-  build: path.join(__dirname, './dist')
-};
-
-
 module.exports = {
-  entry: {
-    wonder: PATHS.src + '/wonder'
-  },
+  entry: "./src/wonder.ts",
   output: {
-    path: PATHS.build,
-    filename: '[name].js',
-    library: 'Wonder',
-    libraryTarget: 'umd'
+      filename: "./dist/bundle.js",
   },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader'
-      }
-    ]
-  },
-  externals: {
-    MessagingStub: 'MessagingStub'
 
-    //MessagingStub: /^(http|ftp|https):\/\/.*\.js/
-        /*function(context, request, callback) {
-      if (/^(http|ftp|https):\/\/.*.js/.test(request)){
-        console.log(request);
-        return callback(null, 'commonjs ' + request);
-      }
-      callback();
-    }*/
-  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
   resolve: {
-    extensions: ['.ts', '.js']
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
-  plugins: [
-    new WebpackBuildNotifierPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+
+  module: {
+      loaders: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      ],
+
+      preLoaders: [
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { test: /\.js$/, loader: "source-map-loader" }
+      ]
+  },
+
+  // Other options...
 };
