b81b2231efbbb2875f9dda0224cd431288cdd6cd
Danny Koppenhagen <mail@d-koppenhagen.de>
2019-05-05 17:27:09 +0200

    Inlucdes WONDER in an Angular CLI library

--- demo2/index.js
+++
@@ -1,9 +0,0 @@
-var wonder = new window.Wonder.Wonder();
-
-wonder.login('user@localhost:9110')
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
---
+++ projects/wonder/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of wonder
+ */
+
+export * from './lib/wonder';
