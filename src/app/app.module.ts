0c1c5c8e80585824cf8fd2599ffdefb44fefda95
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-04-05 18:06:26 +0200

    chore: updat to angular9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { VideoBoxComponent } from './video-box/video-box.component';
